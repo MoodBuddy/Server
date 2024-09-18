dcbf1363c44a9f7ec8355cbde6565de45e43c420
김멍몽 <wjdahrrla@gmail.com>
2024-09-18 20:29:58 +0900

    refactor: ErrorCode 부분 404 -> 400으로 다 변경

--- src/main/java/moodbuddy/moodbuddy/global/common/exception/ErrorCode.java
+++ src/main/java/moodbuddy/moodbuddy/global/common/exception/ErrorCode.java
@@ -13,10 +13,10 @@ public enum ErrorCode {
     /** 4XX **/
     EMAIL_DUPLICATION(400,"MEMBER-ERR-400","EMAIL DUPLICATED"),
     BAD_REQUEST_SUMMARY(400, "DIARY-ERR-400", "일기가 요약하기에 불충분합니다."),
-    NOT_FOUND(404,"COMMON-ERR-400","PAGE NOT FOUND"),
-    NOT_FOUND_DIARY(404, "DIARY-ERR-400", "일기를 찾을 수 없습니다."),
-    NOT_FOUND_QUDDYTI(404, "QUDDYTI-ERR-400", "쿼디티아이를 찾을 수 없습니다."),
-    NOT_FOUND_USER(404, "USER-ERR-400", "아이디에 해당하는 회원을 찾을 수 없습니다."),
+    NOT_FOUND(400,"COMMON-ERR-400","PAGE NOT FOUND"),
+    NOT_FOUND_DIARY(400, "DIARY-ERR-400", "일기를 찾을 수 없습니다."),
+    NOT_FOUND_QUDDYTI(400, "QUDDYTI-ERR-400", "쿼디티아이를 찾을 수 없습니다."),
+    NOT_FOUND_USER(400, "USER-ERR-400", "아이디에 해당하는 회원을 찾을 수 없습니다."),
     NO_ACCESS_DIARY(403, "DIARY-ERR-403", "접근할 수 없는 일기입니다."),
     TODAY_EXISTING_DIARY(409, "DIARY-ERR-409", "오늘 이미 일기를 작성했습니다."),
 
