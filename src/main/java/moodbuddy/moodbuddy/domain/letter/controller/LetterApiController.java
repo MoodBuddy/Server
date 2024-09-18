54681a7ca80b8e35fb0f14a38f7e416f97ea695a
김멍몽 <wjdahrrla@gmail.com>
2024-09-18 21:04:09 +0900

    refactor: Controller 계층 불필요한 Swagger 어노테이션 정리 - #mung

--- src/main/java/moodbuddy/moodbuddy/domain/letter/controller/LetterApiController.java
+++ src/main/java/moodbuddy/moodbuddy/domain/letter/controller/LetterApiController.java
@@ -13,10 +13,7 @@ import moodbuddy.moodbuddy.domain.letter.dto.response.LetterResDetailsDTO;
 import moodbuddy.moodbuddy.domain.letter.dto.response.LetterResPageDTO;
 import moodbuddy.moodbuddy.domain.letter.dto.response.LetterResSaveDTO;
 import moodbuddy.moodbuddy.domain.letter.service.LetterService;
-import moodbuddy.moodbuddy.global.common.response.ApiPageResponse;
-import moodbuddy.moodbuddy.global.common.response.ApiResponse;
 import moodbuddy.moodbuddy.global.common.util.JwtUtil;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.Timer;
@@ -32,18 +29,12 @@ public class LetterApiController {
 
     @GetMapping("")
     @Operation(summary = "고민상담소 첫 페이지", description = "고민상담소의 첫 페이지로 이동합니다.")
-    @ApiResponses(value = {
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "201", description = "SUCCESS", content = @Content(schema = @Schema(implementation = LetterResPageDTO.class))),
-    })
     public ResponseEntity<?> letterPage(){
         return ResponseEntity.ok(letterService.letterPage());
     }
 
     @PostMapping("/alarm")
     @Operation(summary = "고민상담소 알람 설정", description = "사용자가 작성한 고민 편지에 대한 답장의 알람을 설정합니다.")
-    @ApiResponses(value = {
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "201", description = "SUCCESS", content = @Content(schema = @Schema(implementation = LetterResSaveDTO.class)))
-    })
     public ResponseEntity<?> updateLetterAlarm(
             @Parameter(description = "편지의 letterId")
             @RequestBody LetterReqUpdateDTO letterReqUpdateDTO
@@ -53,9 +44,6 @@ public class LetterApiController {
 
     @PostMapping("/write")
     @Operation(summary = "고민상담소 편지 등록", description = "사용자가 작성한 고민 편지를 등록합니다.")
-    @ApiResponses(value = {
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "201", description = "SUCCESS", content = @Content(schema = @Schema(implementation = LetterResSaveDTO.class))),
-    })
     public ResponseEntity<LetterResSaveDTO> write(
             @Parameter(description = "사용자가 작성한 고민 편지와 답장 형식을 담고 있는 DTO")
             @RequestBody LetterReqDTO letterReqDTO
@@ -82,9 +70,6 @@ public class LetterApiController {
 
     @GetMapping("/details/{letterId}")
     @Operation(summary = "고민상담소 편지 내용", description = "사용자가 작성한 편지 내용과 그에 대한 쿼디의 답변 내용을 보여줍니다.")
-    @ApiResponses(value = {
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "201", description = "SUCCESS", content = @Content(schema = @Schema(implementation = LetterResDetailsDTO.class)))
-    })
     public ResponseEntity<?> details(
             @Parameter(description = "편지의 letterId")
             @PathVariable("letterId") Long letterId
