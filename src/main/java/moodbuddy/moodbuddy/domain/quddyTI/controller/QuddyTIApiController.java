e479c7ed40754a092b43307823d0f2103bad1416
김멍몽 <wjdahrrla@gmail.com>
2024-09-18 21:02:26 +0900

    refactor: QuddyTIApiController ApiResponse 삭제 후 모두 수정, 로그 삭제 - #mung

--- src/main/java/moodbuddy/moodbuddy/domain/quddyTI/controller/QuddyTIApiController.java
+++ src/main/java/moodbuddy/moodbuddy/domain/quddyTI/controller/QuddyTIApiController.java
@@ -1,16 +1,11 @@
 package moodbuddy.moodbuddy.domain.quddyTI.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import moodbuddy.moodbuddy.domain.quddyTI.dto.response.QuddyTIResDetailDTO;
 import moodbuddy.moodbuddy.domain.quddyTI.service.QuddyTIService;
-import moodbuddy.moodbuddy.global.common.response.ApiResponse;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
