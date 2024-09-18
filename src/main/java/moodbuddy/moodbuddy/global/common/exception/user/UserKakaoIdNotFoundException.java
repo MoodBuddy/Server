ab76957eef753da1403c6162cf9c697669285e01
김멍몽 <wjdahrrla@gmail.com>
2024-09-18 20:28:29 +0900

    refactor: Exception NotFound -> 404 말고 400 에러로 변경 - #mung

--- src/main/java/moodbuddy/moodbuddy/global/common/exception/user/UserKakaoIdNotFoundException.java
+++ src/main/java/moodbuddy/moodbuddy/global/common/exception/user/UserKakaoIdNotFoundException.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 import moodbuddy.moodbuddy.global.common.exception.ErrorCode;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
-@ResponseStatus(HttpStatus.NOT_FOUND)
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 @Getter
 public class UserKakaoIdNotFoundException extends RuntimeException {
     private final ErrorCode errorCode;
