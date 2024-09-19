a98ac75cc5c310e27452722866c5f0e1faa7171c
zzammin <woalsdl7399@naver.com>
2024-09-20 02:02:53 +0900

    Fix : sms 관련 user, letter 부분 수정 - #zzam

--- src/main/java/moodbuddy/moodbuddy/domain/sms/service/SmsServiceImpl.java
+++ src/main/java/moodbuddy/moodbuddy/domain/sms/service/SmsServiceImpl.java
@@ -1,6 +1,5 @@
 package moodbuddy.moodbuddy.domain.sms.service;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.nurigo.sdk.NurigoApp;
 import net.nurigo.sdk.message.exception.NurigoMessageNotReceivedException;
@@ -20,15 +19,26 @@ public class SmsServiceImpl implements SmsService{
     private String senderPhone;
 
     private final DefaultMessageService messageService;
+    private static final String USER_MESSAGE = "[moodbuddy] 일기를 작성할 시간이에요! 오늘의 소중한 순간을 쿼디와 함께 기록해볼까요?";
+    private static final String LETTER_MESSAGE = "[moodbuddy] 쿼디의 고민 편지 답장이 도착했어요! 어서 확인해보세요 :)";
 
     public SmsServiceImpl(){
         this.messageService = NurigoApp.INSTANCE.initialize(smsApiKey, smsApiSecretKey, "https://api.coolsms.co.kr");
     }
 
     @Override
-    public void sendMessage(String to){
+    public void sendMessage(String to, String messageType){
         try {
-            messageService.send(setMessage(to)); // 컨트롤러에서 30초 이후에 자동으로 호출하니까 sms는 따로 지연 시간 설정할 필요 X
+            switch (messageType){
+                case "USER" :
+                    messageService.send(setMessage(to, USER_MESSAGE));
+                    break;
+                case "LETTER" :
+                    messageService.send(setMessage(to, LETTER_MESSAGE));
+                    break;
+                default :
+                    throw new IllegalArgumentException("Invalid message type.");
+            }
         } catch (NurigoMessageNotReceivedException exception) {
             // 발송에 실패한 메시지 목록을 확인
             log.error("exception.getFailedMessageList() : "+exception.getFailedMessageList());
@@ -38,11 +48,11 @@ public class SmsServiceImpl implements SmsService{
         }
     }
 
-    private Message setMessage(String to){
+    private Message setMessage(String to, String text){
         Message message = new Message();
         message.setFrom(senderPhone);
         message.setTo(to);
-        message.setText("[moodbuddy] 쿼디의 고민 편지 답장이 도착했어요! 어서 확인해보세요 :)");
+        message.setText(text);
 
         return message;
     }
