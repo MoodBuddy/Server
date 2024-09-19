3eb4a19301bd75ccadec3c501681cfb45b16b845
김멍몽 <wjdahrrla@gmail.com>
2024-09-19 17:02:07 +0900

    fix: 테이블 구조 브릿지 형태로 수정 및 KakaoId -> UserId 기준으로 모든 코드 변경 5차 완료 - #mung

--- src/main/java/moodbuddy/moodbuddy/domain/bookMark/entity/BookMark.java
+++ src/main/java/moodbuddy/moodbuddy/domain/bookMark/entity/BookMark.java
@@ -10,11 +10,12 @@ import moodbuddy.moodbuddy.global.common.base.BaseEntity;
 @Getter
 @Builder
 @AllArgsConstructor
+@Table(name = "book_mark")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class BookMark extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "bookmark_id")
+    @Column(name = "book_mark_id")
     private Long id;
 
     @Column(name = "user_id", nullable = false, columnDefinition = "bigint")
