745fc8030b1d07878cf32ef66893809e6c7afe7e
김멍몽 <wjdahrrla@gmail.com>
2024-09-22 15:40:22 +0900

    refactor: QuddyTIResDetailDTO 전달 데이터 수정 - #mung

--- src/main/java/moodbuddy/moodbuddy/domain/quddyTI/dto/response/QuddyTIResDetailDTO.java
+++ src/main/java/moodbuddy/moodbuddy/domain/quddyTI/dto/response/QuddyTIResDetailDTO.java
@@ -1,37 +1,29 @@
 package moodbuddy.moodbuddy.domain.quddyTI.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-//@Builder
+@AllArgsConstructor
+@Builder
 public class QuddyTIResDetailDTO {
-    @Schema(description = "사용자 고유 식별자(userId)", example = "2")
     private Long userId;
-    @Schema(description = "Happiness 갯수", example = "2")
+    private String quddyTIYearMonth;
+    private Integer diaryFrequency;
     private Integer happinessCount;
-    @Schema(description = "Anger 갯수", example = "2")
     private Integer angerCount;
-    @Schema(description = "Disgust 갯수", example = "2")
     private Integer disgustCount;
-    @Schema(description = "Fear 갯수", example = "2")
     private Integer fearCount;
-    @Schema(description = "Neutral 갯수", example = "2")
     private Integer neutralCount;
-    @Schema(description = "Sadness 갯수", example = "2")
     private Integer sadnessCount;
-    @Schema(description = "Surprise 갯수", example = "2")
     private Integer surpriseCount;
-    @Schema(description = "Daily 갯수", example = "2")
     private Integer dailyCount;
-    @Schema(description = "Growth 갯수", example = "2")
     private Integer growthCount;
-    @Schema(description = "Emotion 갯수", example = "2")
     private Integer emotionCount;
-    @Schema(description = "Travel 갯수", example = "2")
     private Integer travelCount;
-    @Schema(description = "쿼디티아이", example = "PEH")
     private String quddyTIType ;
 }
