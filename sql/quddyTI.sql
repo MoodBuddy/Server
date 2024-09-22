6116f51082305889b591b848a1c07f3cc3000e2d
김멍몽 <wjdahrrla@gmail.com>
2024-09-22 15:38:25 +0900

    refactor: sql 수정 - #mung

--- sql/quddyTI.sql
+++ sql/quddyTI.sql
@@ -1,3 +1,11 @@
+-- 7월
+INSERT INTO quddy_ti (
+    user_id, diary_frequency, quddy_ti_year_month, happiness_count, anger_count, disgust_count, fear_count, neutral_count, sadness_count, surprise_count,
+    daily_count, growth_count, emotion_count, travel_count, quddy_ti_type, created_time, updated_time
+) VALUES
+    (1, 31, "2024-07", 11, 2, 2, 1, 8, 4, 3, 15, 5, 7, 4, 'JDH', NOW(), NOW());
+
+-- 8월
 INSERT INTO quddy_ti (
     user_id, diary_frequency, quddy_ti_year_month, happiness_count, anger_count, disgust_count, fear_count, neutral_count, sadness_count, surprise_count,
     daily_count, growth_count, emotion_count, travel_count, quddy_ti_type, created_time, updated_time
