ea51bde4f52616e85a1f106101d17d38ea3ed869
김멍몽 <wjdahrrla@gmail.com>
2024-09-22 16:12:56 +0900

    feat: quddyTI에 quddyTIStatus 추가 - #mung

--- sql/quddyTI.sql
+++ sql/quddyTI.sql
@@ -1,13 +1,20 @@
 -- 7월
 INSERT INTO quddy_ti (
     user_id, diary_frequency, quddy_ti_year_month, happiness_count, anger_count, disgust_count, fear_count, neutral_count, sadness_count, surprise_count,
-    daily_count, growth_count, emotion_count, travel_count, quddy_ti_type, created_time, updated_time
+    daily_count, growth_count, emotion_count, travel_count, quddy_ti_type, quddy_ti_status, created_time, updated_time
 ) VALUES
-    (1, 31, "2024-07", 11, 2, 2, 1, 8, 4, 3, 15, 5, 7, 4, 'JDH', NOW(), NOW());
+    (1, 31, '2024-07', 11, 2, 2, 1, 8, 4, 3, 15, 5, 7, 4, 'JDH', 'FINISH', NOW(), NOW());
 
 -- 8월
 INSERT INTO quddy_ti (
     user_id, diary_frequency, quddy_ti_year_month, happiness_count, anger_count, disgust_count, fear_count, neutral_count, sadness_count, surprise_count,
-    daily_count, growth_count, emotion_count, travel_count, quddy_ti_type, created_time, updated_time
+    daily_count, growth_count, emotion_count, travel_count, quddy_ti_type, quddy_ti_status, created_time, updated_time
 ) VALUES
-    (1, 31, "2024-08", 12, 3, 1, 2, 9, 3, 1, 15, 5, 7, 4, 'JDH', NOW(), NOW());
+    (1, 31, '2024-08', 12, 3, 1, 2, 9, 3, 1, 15, 5, 7, 4, 'JDH', 'FINISH', NOW(), NOW());
+
+-- 9월
+INSERT INTO quddy_ti (
+    user_id, diary_frequency, quddy_ti_year_month, happiness_count, anger_count, disgust_count, fear_count, neutral_count, sadness_count, surprise_count,
+    daily_count, growth_count, emotion_count, travel_count, quddy_ti_type, quddy_ti_status, created_time, updated_time
+) VALUES
+    (1, 0, '2024-09', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 'CREATING', NOW(), NOW());
