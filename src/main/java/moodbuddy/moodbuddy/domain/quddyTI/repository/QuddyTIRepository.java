67c86431a91f2f6f4d655beb8b835523ecf62bdf
김멍몽 <wjdahrrla@gmail.com>
2024-09-22 15:49:10 +0900

    refactor: findAll을 QuddyTiResDetailDTO -> List<QuddyTIResDetailDTO> 로 변경 - #mung

--- src/main/java/moodbuddy/moodbuddy/domain/quddyTI/repository/QuddyTIRepository.java
+++ src/main/java/moodbuddy/moodbuddy/domain/quddyTI/repository/QuddyTIRepository.java
@@ -3,8 +3,10 @@ package moodbuddy.moodbuddy.domain.quddyTI.repository;
 import moodbuddy.moodbuddy.domain.quddyTI.entity.QuddyTI;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface QuddyTIRepository extends JpaRepository<QuddyTI, Long>, QuddyTIRepositoryCustom {
-    Optional<QuddyTI> findByUserId(Long userId);
+//    Optional<QuddyTI> findByUserId(Long userId);
+    Optional<List<QuddyTI>> findByUserId(Long userId);
 }
--- src/main/java/moodbuddy/moodbuddy/domain/quddyTI/service/QuddyTIService.java
+++ src/main/java/moodbuddy/moodbuddy/domain/quddyTI/service/QuddyTIService.java
@@ -3,8 +3,10 @@ package moodbuddy.moodbuddy.domain.quddyTI.service;
 import moodbuddy.moodbuddy.domain.quddyTI.dto.response.QuddyTIResDetailDTO;
 import moodbuddy.moodbuddy.domain.quddyTI.entity.QuddyTI;
 
+import java.util.List;
+
 public interface QuddyTIService {
     void aggregateAndSaveDiaryData();
     void saveQuddyTI(QuddyTI quddyTI);
-    QuddyTIResDetailDTO findAll();
+    List<QuddyTIResDetailDTO> findAll();
 }
