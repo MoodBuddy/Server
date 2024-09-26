5fee0cc4b5d25af89d45de854019f2f3a9a639e8
김멍몽 <wjdahrrla@gmail.com>
2024-09-26 15:24:23 +0900

    docs: draftDraft.sql 임시저장 일기 더미데이터 생성 - #mung

---
+++ sql/draftDiary.sql
@@ -0,0 +1,17 @@
+INSERT INTO diary (diary_title, diary_date, diary_content, diary_weather, diary_emotion, diary_status, diary_subject, diary_summary, user_id, diary_book_mark_check, diary_font, diary_font_size, created_time, updated_time)
+VALUES
+    ('임시 일기 1', '2023-09-01', '임시 저장된 일기 내용 1', 'CLEAR', null, 'DRAFT', null, null, 1, 0, 'INTER', 'PX24', NOW(), NOW()),
+    ('임시 일기 2', '2023-09-02', '임시 저장된 일기 내용 2', 'CLOUDY', null, 'DRAFT', null, null, 1, 0, 'MEETME', 'PX28', NOW(), NOW()),
+    ('임시 일기 3', '2023-09-03', '임시 저장된 일기 내용 3', 'RAIN', null, 'DRAFT', null, null, 1, 0, 'INTER', 'PX30', NOW(), NOW()),
+    ('임시 일기 4', '2023-09-04', '임시 저장된 일기 내용 4', 'CLEAR', null, 'DRAFT', null, null, 1, 0, 'MEETME', 'PX24', NOW(), NOW()),
+    ('임시 일기 5', '2023-09-05', '임시 저장된 일기 내용 5', 'SNOW', null, 'DRAFT', null, null, 1, 0, 'INTER', 'PX28', NOW(), NOW()),
+    ('임시 일기 6', '2023-09-06', '임시 저장된 일기 내용 6', 'CLOUDY', null, 'DRAFT', null, null, 1, 0, 'MEETME', 'PX30', NOW(), NOW()),
+    ('임시 일기 7', '2023-09-07', '임시 저장된 일기 내용 7', 'RAIN', null, 'DRAFT', null, null, 1, 0, 'INTER', 'PX24', NOW(), NOW()),
+    ('임시 일기 8', '2023-09-08', '임시 저장된 일기 내용 8', 'CLEAR', null, 'DRAFT', null, null, 1, 0, 'MEETME', 'PX28', NOW(), NOW()),
+    ('임시 일기 9', '2023-09-09', '임시 저장된 일기 내용 9', 'SNOW', null, 'DRAFT', null, null, 1, 0, 'INTER', 'PX30', NOW(), NOW()),
+    ('임시 일기 10', '2023-09-10', '임시 저장된 일기 내용 10', 'CLOUDY', null, 'DRAFT', null, null, 1, 0, 'MEETME', 'PX24', NOW(), NOW()),
+    ('임시 일기 11', '2023-09-11', '임시 저장된 일기 내용 11', 'RAIN', null, 'DRAFT', null, null, 1, 0, 'INTER', 'PX28', NOW(), NOW()),
+    ('임시 일기 12', '2023-09-12', '임시 저장된 일기 내용 12', 'CLEAR', null, 'DRAFT', null, null, 1, 0, 'MEETME', 'PX30', NOW(), NOW()),
+    ('임시 일기 13', '2023-09-13', '임시 저장된 일기 내용 13', 'SNOW', null, 'DRAFT', null, null, 1, 0, 'INTER', 'PX24', NOW(), NOW()),
+    ('임시 일기 14', '2023-09-14', '임시 저장된 일기 내용 14', 'CLOUDY', null, 'DRAFT', null, null, 1, 0, 'MEETME', 'PX28', NOW(), NOW()),
+    ('임시 일기 15', '2023-09-15', '임시 저장된 일기 내용 15', 'RAIN', null, 'DRAFT', null, null, 1, 0, 'INTER', 'PX30', NOW(), NOW());
