d1d285f870cdeb3431242eaf3f86f1ea826cd368
@_.mung <wjdahrrla@gmail.com>
2024-11-11 11:45:22 +0900

    Refactor: BookMark DTO를 record로 리팩토링해라 - #8
    BookMark DTO를 record로 리팩토링함.
    
    리팩토링 내용:
    - class -> record로 변경
    - 변경에 따른 관련 코드 수정

--- src/main/java/moodbuddy/moodbuddy/domain/diary/dto/request/DiaryReqDraftSelectDeleteDTO.java
+++ src/main/java/moodbuddy/moodbuddy/domain/diary/dto/request/DiaryReqDraftSelectDeleteDTO.java
@@ -2,11 +2,6 @@ package moodbuddy.moodbuddy.domain.diary.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.util.List;
 
 public record DiaryReqDraftSelectDeleteDTO (
