c9de8a4c1ca2bc553b3e341425973744d3df7a7f
@_.mung <wjdahrrla@gmail.com>
2024-11-21 23:17:08 +0900

    Refactor: DiaryFacadeImpl 메서드명 리팩토링해라 - #21

--- src/main/java/moodbuddy/moodbuddy/domain/diary/facade/DiaryFacade.java
+++ src/main/java/moodbuddy/moodbuddy/domain/diary/facade/DiaryFacade.java
@@ -3,14 +3,10 @@ package moodbuddy.moodbuddy.domain.diary.facade;
 import moodbuddy.moodbuddy.domain.diary.dto.request.DiaryReqSaveDTO;
 import moodbuddy.moodbuddy.domain.diary.dto.request.DiaryReqUpdateDTO;
 import moodbuddy.moodbuddy.domain.diary.dto.response.DiaryResDetailDTO;
-import moodbuddy.moodbuddy.domain.diary.dto.response.image.DiaryImageResURLDTO;
-import moodbuddy.moodbuddy.global.common.cloud.dto.request.CloudReqDTO;
-
-import java.io.IOException;
 
 public interface DiaryFacade {
-    DiaryResDetailDTO saveDiary(DiaryReqSaveDTO requestDTO);
-    DiaryResDetailDTO updateDiary(DiaryReqUpdateDTO requestDTO);
-    void deleteDiary(final Long diaryId);
+    DiaryResDetailDTO save(DiaryReqSaveDTO requestDTO);
+    DiaryResDetailDTO update(DiaryReqUpdateDTO requestDTO);
+    void delete(final Long diaryId);
     DiaryResDetailDTO findOneByDiaryId(final Long diaryId);
 }
