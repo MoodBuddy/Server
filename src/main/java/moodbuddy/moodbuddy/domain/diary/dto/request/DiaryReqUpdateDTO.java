0efa78d88ec5c38f9ba144b2caf827707a0e87e1
@_.mung <wjdahrrla@gmail.com>
2024-11-22 00:25:46 +0900

    Fix: 일기 수정 시 이미지 업로드 실패에 대한 에러 해결해라 - #21
    해결한 내용:
    - 일기 수정 시 기존 이미지 제거
    - 일기 수정 시 새로운 이미지 받아와 저장

--- src/main/java/moodbuddy/moodbuddy/domain/diary/dto/request/DiaryReqUpdateDTO.java
+++ src/main/java/moodbuddy/moodbuddy/domain/diary/dto/request/DiaryReqUpdateDTO.java
@@ -28,8 +28,6 @@ public record DiaryReqUpdateDTO (
         @Schema(description = "일기 폰트 사이즈", example = "PX30")
         DiaryFontSize diaryFontSize,
         @Schema(description = "새로운 이미지", example = "[이미지 URL, 이미지 URL]")
-        List<String> newImageURLs,
-        @Schema(description = "삭제할 이미지", example = "[이미지 URL, 이미지 URL]")
-        List<String> deleteImageURLs
+        List<String> newImageURLs
 ) {
 }
