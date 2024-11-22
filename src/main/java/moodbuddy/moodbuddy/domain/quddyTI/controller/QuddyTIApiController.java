ccfb12e71e6e7c67437d8b521c6b00058b481562
@_.mung <wjdahrrla@gmail.com>
2024-11-22 19:02:19 +0900

    Refactor: QuddyTI의 기존 코드에 퍼사드패턴 적용해라 - #10
    QuddyTI의 기존 코드에 퍼사드패턴 적용함.
    
    리팩토링 내용:
    - 퍼사트 패턴을 적용

--- src/main/java/moodbuddy/moodbuddy/domain/quddyTI/controller/QuddyTIApiController.java
+++ src/main/java/moodbuddy/moodbuddy/domain/quddyTI/controller/QuddyTIApiController.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import moodbuddy.moodbuddy.domain.quddyTI.dto.response.QuddyTIResDetailDTO;
+import moodbuddy.moodbuddy.domain.quddyTI.facade.QuddyTIFacade;
 import moodbuddy.moodbuddy.domain.quddyTI.service.QuddyTIService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,17 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/member/quddyTI")
+@RequestMapping("/api/v2/member/quddyTI")
 @Tag(name = "QuddyTI", description = "쿼디티아이 관련 API")
 @RequiredArgsConstructor
 @Slf4j
 public class QuddyTIApiController {
-    private final QuddyTIService quddyTIService;
+    private final QuddyTIFacade quddyTIFacade;
 
     /** 구현 완료 **/
     @GetMapping("/findAll")
     @Operation(summary = "쿼디티아이 조회", description = "쿼디티아이 관련 내용을 모두 조회합니다.")
     public ResponseEntity<List<QuddyTIResDetailDTO>> findAll() {
-        return ResponseEntity.ok().body(quddyTIService.findAll());
+        return ResponseEntity.ok().body(quddyTIFacade.findAll());
     }
 }
--- src/main/java/moodbuddy/moodbuddy/domain/quddyTI/service/QuddyTIService.java
+++ src/main/java/moodbuddy/moodbuddy/domain/quddyTI/service/QuddyTIService.java
@@ -1,10 +1,18 @@
 package moodbuddy.moodbuddy.domain.quddyTI.service;
 
+import moodbuddy.moodbuddy.domain.diary.domain.type.DiaryEmotion;
+import moodbuddy.moodbuddy.domain.diary.domain.type.DiarySubject;
 import moodbuddy.moodbuddy.domain.quddyTI.dto.response.QuddyTIResDetailDTO;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 
 public interface QuddyTIService {
-    void aggregateAndSaveDiaryData();
-    List<QuddyTIResDetailDTO> findAll();
+    List<QuddyTIResDetailDTO> findAll(final Long userId);
+    void createNewMonthQuddyTI(Long userId, LocalDate currentMonth);
+    void updateLastMonthQuddyTI(Long userId, LocalDate start,
+                                Map<DiaryEmotion, Long> emotionCounts,
+                                Map<DiarySubject, Long> subjectCounts,
+                                String quddyTIType);
 }
