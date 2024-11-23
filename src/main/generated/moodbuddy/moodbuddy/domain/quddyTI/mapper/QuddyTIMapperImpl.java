bc23be2423acac5ba01d05fc55f41d21f761938f
@_.mung <wjdahrrla@gmail.com>
2024-11-23 17:54:02 +0900

    Refactor: QuddyTIResDetailDTO를 Record를 활용해라 - #10

--- src/main/generated/moodbuddy/moodbuddy/domain/quddyTI/mapper/QuddyTIMapperImpl.java
+++ src/main/generated/moodbuddy/moodbuddy/domain/quddyTI/mapper/QuddyTIMapperImpl.java
@@ -3,11 +3,12 @@ package moodbuddy.moodbuddy.domain.quddyTI.mapper;
 import javax.annotation.processing.Generated;
 import moodbuddy.moodbuddy.domain.quddyTI.domain.QuddyTI;
 import moodbuddy.moodbuddy.domain.quddyTI.dto.response.QuddyTIResDetailDTO;
+import moodbuddy.moodbuddy.global.common.base.MoodBuddyStatus;
 import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-11-23T17:43:39+0900",
+    date = "2024-11-23T17:53:28+0900",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.13 (Homebrew)"
 )
 @Component
@@ -19,26 +20,44 @@ public class QuddyTIMapperImpl implements QuddyTIMapper {
             return null;
         }
 
-        QuddyTIResDetailDTO.QuddyTIResDetailDTOBuilder quddyTIResDetailDTO = QuddyTIResDetailDTO.builder();
+        Long userId = null;
+        String quddyTIYear = null;
+        String quddyTIMonth = null;
+        Integer diaryFrequency = null;
+        Integer happinessCount = null;
+        Integer angerCount = null;
+        Integer disgustCount = null;
+        Integer fearCount = null;
+        Integer neutralCount = null;
+        Integer sadnessCount = null;
+        Integer surpriseCount = null;
+        Integer dailyCount = null;
+        Integer growthCount = null;
+        Integer emotionCount = null;
+        Integer travelCount = null;
+        String quddyTIType = null;
+        MoodBuddyStatus moodBuddyStatus = null;
 
-        quddyTIResDetailDTO.userId( quddyTI.getUserId() );
-        quddyTIResDetailDTO.quddyTIYear( quddyTI.getQuddyTIYear() );
-        quddyTIResDetailDTO.quddyTIMonth( quddyTI.getQuddyTIMonth() );
-        quddyTIResDetailDTO.diaryFrequency( quddyTI.getDiaryFrequency() );
-        quddyTIResDetailDTO.happinessCount( quddyTI.getHappinessCount() );
-        quddyTIResDetailDTO.angerCount( quddyTI.getAngerCount() );
-        quddyTIResDetailDTO.disgustCount( quddyTI.getDisgustCount() );
-        quddyTIResDetailDTO.fearCount( quddyTI.getFearCount() );
-        quddyTIResDetailDTO.neutralCount( quddyTI.getNeutralCount() );
-        quddyTIResDetailDTO.sadnessCount( quddyTI.getSadnessCount() );
-        quddyTIResDetailDTO.surpriseCount( quddyTI.getSurpriseCount() );
-        quddyTIResDetailDTO.dailyCount( quddyTI.getDailyCount() );
-        quddyTIResDetailDTO.growthCount( quddyTI.getGrowthCount() );
-        quddyTIResDetailDTO.emotionCount( quddyTI.getEmotionCount() );
-        quddyTIResDetailDTO.travelCount( quddyTI.getTravelCount() );
-        quddyTIResDetailDTO.quddyTIType( quddyTI.getQuddyTIType() );
-        quddyTIResDetailDTO.moodBuddyStatus( quddyTI.getMoodBuddyStatus() );
+        userId = quddyTI.getUserId();
+        quddyTIYear = quddyTI.getQuddyTIYear();
+        quddyTIMonth = quddyTI.getQuddyTIMonth();
+        diaryFrequency = quddyTI.getDiaryFrequency();
+        happinessCount = quddyTI.getHappinessCount();
+        angerCount = quddyTI.getAngerCount();
+        disgustCount = quddyTI.getDisgustCount();
+        fearCount = quddyTI.getFearCount();
+        neutralCount = quddyTI.getNeutralCount();
+        sadnessCount = quddyTI.getSadnessCount();
+        surpriseCount = quddyTI.getSurpriseCount();
+        dailyCount = quddyTI.getDailyCount();
+        growthCount = quddyTI.getGrowthCount();
+        emotionCount = quddyTI.getEmotionCount();
+        travelCount = quddyTI.getTravelCount();
+        quddyTIType = quddyTI.getQuddyTIType();
+        moodBuddyStatus = quddyTI.getMoodBuddyStatus();
 
-        return quddyTIResDetailDTO.build();
+        QuddyTIResDetailDTO quddyTIResDetailDTO = new QuddyTIResDetailDTO( userId, quddyTIYear, quddyTIMonth, diaryFrequency, happinessCount, angerCount, disgustCount, fearCount, neutralCount, sadnessCount, surpriseCount, dailyCount, growthCount, emotionCount, travelCount, quddyTIType, moodBuddyStatus );
+
+        return quddyTIResDetailDTO;
     }
 }
