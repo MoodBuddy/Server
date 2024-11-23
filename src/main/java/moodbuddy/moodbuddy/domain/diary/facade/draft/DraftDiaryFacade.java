7bd475dd9383d4bf2390e2ee3ab0dae86419f013
@_.mung <wjdahrrla@gmail.com>
2024-11-23 22:06:56 +0900

    Refactor: DraftDiary DTO 명칭 변경 및 불필요 제거 - #24

--- src/main/java/moodbuddy/moodbuddy/domain/diary/facade/draft/DraftDiaryFacade.java
+++ src/main/java/moodbuddy/moodbuddy/domain/diary/facade/draft/DraftDiaryFacade.java
@@ -1,13 +1,15 @@
 package moodbuddy.moodbuddy.domain.diary.facade.draft;
 
-import moodbuddy.moodbuddy.domain.diary.dto.request.draft.DiaryReqDraftSelectDeleteDTO;
+import moodbuddy.moodbuddy.domain.diary.dto.request.draft.DraftDiaryReqSelectDeleteDTO;
 import moodbuddy.moodbuddy.domain.diary.dto.request.DiaryReqSaveDTO;
 import moodbuddy.moodbuddy.domain.diary.dto.response.DiaryResDetailDTO;
-import moodbuddy.moodbuddy.domain.diary.dto.response.draft.DiaryResDraftFindAllDTO;
+import moodbuddy.moodbuddy.domain.diary.dto.response.draft.DraftDiaryResFindOneDTO;
+
+import java.util.List;
 
 public interface DraftDiaryFacade {
     DiaryResDetailDTO save(DiaryReqSaveDTO requestDTO);
-    DiaryResDraftFindAllDTO findAll();
-    void selectDelete(DiaryReqDraftSelectDeleteDTO requestDTO);
+    List<DraftDiaryResFindOneDTO> findAll();
+    void selectDelete(DraftDiaryReqSelectDeleteDTO requestDTO);
     DiaryResDetailDTO findOneByDiaryId(Long diaryId);
 }
