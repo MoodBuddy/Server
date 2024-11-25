af68af2170a33ce1744137c12c136913a7949519
@_.mung <wjdahrrla@gmail.com>
2024-11-26 01:52:32 +0900

    Feat: 임시 저장 일기를 일기로 저장하는 기능 구현해라 - #24
    임시 저장 일기를 일기로 저장하는 기능을 구현함.
    
    구현한 내용:
    - 임시 저장 일기를 일기로 변경하는 로직 구현
    - 임시 저장 일기만의 역할을 분리함

--- src/main/java/moodbuddy/moodbuddy/domain/diary/repository/draft/DraftDiaryRepository.java
+++ src/main/java/moodbuddy/moodbuddy/domain/diary/repository/draft/DraftDiaryRepository.java
@@ -5,8 +5,11 @@ import moodbuddy.moodbuddy.domain.diary.domain.type.DiaryStatus;
 import moodbuddy.moodbuddy.global.common.base.MoodBuddyStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 public interface DraftDiaryRepository extends JpaRepository<Diary, Long>, DraftDiaryRepositoryCustom {
     Optional<Diary> findByDiaryIdAndDiaryStatusAndMoodBuddyStatus(Long diaryId, DiaryStatus diaryStatus, MoodBuddyStatus moodBuddyStatus);
+    List<Diary> findAllByDiaryDateAndUserIdAndDiaryStatus(LocalDate diaryDate, Long userId, DiaryStatus diaryStatus);
 }
--- src/main/java/moodbuddy/moodbuddy/domain/diary/service/draft/DraftDiaryService.java
+++ src/main/java/moodbuddy/moodbuddy/domain/diary/service/draft/DraftDiaryService.java
@@ -1,17 +1,19 @@
 package moodbuddy.moodbuddy.domain.diary.service.draft;
 
 import moodbuddy.moodbuddy.domain.diary.domain.Diary;
+import moodbuddy.moodbuddy.domain.diary.dto.request.DiaryReqUpdateDTO;
 import moodbuddy.moodbuddy.domain.diary.dto.request.draft.DraftDiaryReqSelectDeleteDTO;
 import moodbuddy.moodbuddy.domain.diary.dto.request.DiaryReqSaveDTO;
-import moodbuddy.moodbuddy.domain.diary.dto.response.DiaryResDetailDTO;
 import moodbuddy.moodbuddy.domain.diary.dto.response.draft.DraftDiaryResDetailDTO;
 import moodbuddy.moodbuddy.domain.diary.dto.response.draft.DraftDiaryResFindOneDTO;
-
 import java.util.List;
+import java.util.Map;
 
 public interface DraftDiaryService {
     // 일기 임시 저장
     Diary save(DiaryReqSaveDTO diaryReqSaveDTO, final Long userId);
+
+    Diary update(DiaryReqUpdateDTO requestDTO, Map<String, String> stringStringMap, final Long userId);
 
     // 일기 임시 저장 날짜 조회
     List<DraftDiaryResFindOneDTO> findAll(final Long userId);
