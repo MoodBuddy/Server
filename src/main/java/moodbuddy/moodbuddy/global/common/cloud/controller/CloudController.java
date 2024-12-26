a19f767635900365e79c1591db05b9ffe9b19886
@_.mung <wjdahrrla@gmail.com>
2024-12-26 15:49:11 +0900

    Refactor: generate-url api Url 변경해라 - #39

--- src/main/java/moodbuddy/moodbuddy/global/common/cloud/controller/CloudController.java
+++ src/main/java/moodbuddy/moodbuddy/global/common/cloud/controller/CloudController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/member/cloud")
+@RequestMapping("/api/v2/member/cloud")
 @Tag(name = "Cloud", description = "클라우드 관련 API")
 @RequiredArgsConstructor
 public class CloudController {
