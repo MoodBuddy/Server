f394f8ab872eb0a200afbc9b50017c97dabfd8b7
@_.mung <wjdahrrla@gmail.com>
2025-01-02 01:17:05 +0900

    Refactor: DraftDiaryFindRepositoryImpl Querydsl 부분 코드 리팩토링해라 - #43

--- src/main/java/moodbuddy/moodbuddy/domain/diary/dto/response/draft/DraftDiaryResDetailDTO.java
+++ src/main/java/moodbuddy/moodbuddy/domain/diary/dto/response/draft/DraftDiaryResDetailDTO.java
@@ -1,10 +1,7 @@
 package moodbuddy.moodbuddy.domain.diary.dto.response.draft;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import moodbuddy.moodbuddy.domain.diary.domain.type.DiaryFont;
 import moodbuddy.moodbuddy.domain.diary.domain.type.DiaryFontSize;
 import moodbuddy.moodbuddy.domain.diary.domain.type.DiaryStatus;
@@ -13,7 +10,7 @@ import moodbuddy.moodbuddy.global.common.base.MoodBuddyStatus;
 import java.time.LocalDate;
 import java.util.List;
 
-@Data
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
@@ -30,7 +27,7 @@ public class DraftDiaryResDetailDTO {
     private MoodBuddyStatus moodBuddyStatus;
 
     @JsonInclude(JsonInclude.Include.NON_NULL)
-    private List<String> diaryImgList;
+    private List<String> diaryImageUrls;
 
     public DraftDiaryResDetailDTO(Long diaryId, Long userId, String diaryTitle, LocalDate diaryDate, String diaryContent, DiaryWeather diaryWeather, DiaryStatus diaryStatus, DiaryFont diaryFont, DiaryFontSize diaryFontSize, MoodBuddyStatus moodBuddyStatus) {
         this.diaryId = diaryId;
@@ -43,6 +40,10 @@ public class DraftDiaryResDetailDTO {
         this.diaryFont = diaryFont;
         this.diaryFontSize = diaryFontSize;
         this.moodBuddyStatus = moodBuddyStatus;
-        this.diaryImgList = List.of();
+        this.diaryImageUrls = List.of();
+    }
+
+    public void saveDiaryImageUrls(List<String> imageUrls) {
+        this.diaryImageUrls = imageUrls;
     }
 }
