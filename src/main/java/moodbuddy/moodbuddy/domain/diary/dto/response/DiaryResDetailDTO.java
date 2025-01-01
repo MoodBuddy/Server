ef008c33bb86034f772261f0563c95990f1c444a
@_.mung <wjdahrrla@gmail.com>
2025-01-02 01:30:52 +0900

    Refactor: DiaryFindRepositoryImpl Querydsl 부분 코드 리팩토링해라 - #43

--- src/main/java/moodbuddy/moodbuddy/domain/diary/dto/response/DiaryResDetailDTO.java
+++ src/main/java/moodbuddy/moodbuddy/domain/diary/dto/response/DiaryResDetailDTO.java
@@ -1,18 +1,44 @@
 package moodbuddy.moodbuddy.domain.diary.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import moodbuddy.moodbuddy.domain.diary.domain.type.*;
 import java.time.LocalDate;
 import java.util.List;
 
-public record DiaryResDetailDTO(
-        Long diaryId,
-        String diaryTitle,
-        LocalDate diaryDate,
-        String diaryContent,
-        DiaryWeather diaryWeather,
-        DiaryEmotion diaryEmotion,
-        Boolean diaryBookMark,
-        DiaryFont diaryFont,
-        DiaryFontSize diaryFontSize
-//        List<String> diaryImages
-) {}
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class DiaryResDetailDTO {
+    private Long diaryId;
+    private String diaryTitle;
+    private LocalDate diaryDate;
+    private String diaryContent;
+    private DiaryWeather diaryWeather;
+    private DiaryEmotion diaryEmotion;
+    private DiaryFont diaryFont;
+    private DiaryFontSize diaryFontSize;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private List<String> diaryImageUrls;
+
+
+    public DiaryResDetailDTO(Long diaryId, String diaryTitle, LocalDate diaryDate, String diaryContent, DiaryWeather diaryWeather, DiaryEmotion diaryEmotion, DiaryFont diaryFont, DiaryFontSize diaryFontSize) {
+        this.diaryId = diaryId;
+        this.diaryTitle = diaryTitle;
+        this.diaryDate = diaryDate;
+        this.diaryContent = diaryContent;
+        this.diaryWeather = diaryWeather;
+        this.diaryEmotion = diaryEmotion;
+        this.diaryFont = diaryFont;
+        this.diaryFontSize = diaryFontSize;
+        this.diaryImageUrls = List.of();
+    }
+
+    public void saveDiaryImageUrls(List<String> imageUrls) {
+        this.diaryImageUrls = imageUrls;
+    }
+}
--- src/main/java/moodbuddy/moodbuddy/domain/diary/dto/response/draft/DraftDiaryResDetailDTO.java
+++ src/main/java/moodbuddy/moodbuddy/domain/diary/dto/response/draft/DraftDiaryResDetailDTO.java
@@ -2,10 +2,7 @@ package moodbuddy.moodbuddy.domain.diary.dto.response.draft;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
-import moodbuddy.moodbuddy.domain.diary.domain.type.DiaryFont;
-import moodbuddy.moodbuddy.domain.diary.domain.type.DiaryFontSize;
-import moodbuddy.moodbuddy.domain.diary.domain.type.DiaryStatus;
-import moodbuddy.moodbuddy.domain.diary.domain.type.DiaryWeather;
+import moodbuddy.moodbuddy.domain.diary.domain.type.*;
 import moodbuddy.moodbuddy.global.common.base.MoodBuddyStatus;
 import java.time.LocalDate;
 import java.util.List;
@@ -16,30 +13,25 @@ import java.util.List;
 @Builder
 public class DraftDiaryResDetailDTO {
     private Long diaryId;
-    private Long userId;
     private String diaryTitle;
     private LocalDate diaryDate;
     private String diaryContent;
     private DiaryWeather diaryWeather;
-    private DiaryStatus diaryStatus;
+    private DiaryEmotion diaryEmotion;
     private DiaryFont diaryFont;
     private DiaryFontSize diaryFontSize;
-    private MoodBuddyStatus moodBuddyStatus;
-
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private List<String> diaryImageUrls;
 
-    public DraftDiaryResDetailDTO(Long diaryId, Long userId, String diaryTitle, LocalDate diaryDate, String diaryContent, DiaryWeather diaryWeather, DiaryStatus diaryStatus, DiaryFont diaryFont, DiaryFontSize diaryFontSize, MoodBuddyStatus moodBuddyStatus) {
+    public DraftDiaryResDetailDTO(Long diaryId, String diaryTitle, LocalDate diaryDate, String diaryContent, DiaryWeather diaryWeather, DiaryEmotion diaryEmotion, DiaryFont diaryFont, DiaryFontSize diaryFontSize) {
         this.diaryId = diaryId;
-        this.userId = userId;
         this.diaryTitle = diaryTitle;
         this.diaryDate = diaryDate;
         this.diaryContent = diaryContent;
         this.diaryWeather = diaryWeather;
-        this.diaryStatus = diaryStatus;
+        this.diaryEmotion = diaryEmotion;
         this.diaryFont = diaryFont;
         this.diaryFontSize = diaryFontSize;
-        this.moodBuddyStatus = moodBuddyStatus;
         this.diaryImageUrls = List.of();
     }
 
