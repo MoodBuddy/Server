aa3203e92534b75a4a32097b2c00db99adc395f7
@_.mung <wjdahrrla@gmail.com>
2025-01-14 00:33:51 +0900

    Feat: @Validate, @NotNull 어노테이션 추가해라 - #63

--- src/main/java/moodbuddy/moodbuddy/domain/diary/dto/request/update/DiaryReqUpdateDTO.java
+++ src/main/java/moodbuddy/moodbuddy/domain/diary/dto/request/update/DiaryReqUpdateDTO.java
@@ -1,6 +1,7 @@
 package moodbuddy.moodbuddy.domain.diary.dto.request.update;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import moodbuddy.moodbuddy.global.common.base.type.DiaryFont;
 import moodbuddy.moodbuddy.global.common.base.type.DiaryFontSize;
 import moodbuddy.moodbuddy.domain.diary.domain.type.DiaryWeather;
@@ -11,18 +12,25 @@ import java.util.List;
 
 public record DiaryReqUpdateDTO (
         @Schema(description = "수정할 일기 고유 식별자(draftDiaryId)", example = "1")
+        @NotNull
         Long diaryId,
         @Schema(description = "수정할 일기 제목", example = "쿼카의 하카")
+        @NotNull
         String diaryTitle,
         @Schema(description = "일기 날짜", example = "2023-07-02")
+        @NotNull
         LocalDate diaryDate,
         @Schema(description = "수정할 일기 내용", example = "쿼카쿼카쿼카쿼카쿼카쿼카")
+        @NotNull
         String diaryContent,
         @Schema(description = "수정할 일기 날씨(CLEAR, CLOUDY, RAIN, SNOW)", example = "CLEAR")
+        @NotNull
         DiaryWeather diaryWeather,
         @Schema(description = "일기 폰트", example = "INTER")
+        @NotNull
         DiaryFont diaryFont,
         @Schema(description = "일기 폰트 사이즈", example = "PX30")
+        @NotNull
         DiaryFontSize diaryFontSize,
         @Schema(description = "새로운 이미지", example = "[이미지 URL, 이미지 URL]")
         @Nullable
--- src/main/java/moodbuddy/moodbuddy/domain/draftDiary/dto/request/DraftDiaryReqSelectDeleteDTO.java
+++ src/main/java/moodbuddy/moodbuddy/domain/draftDiary/dto/request/DraftDiaryReqSelectDeleteDTO.java
@@ -6,5 +6,6 @@ import java.util.List;
 
 public record DraftDiaryReqSelectDeleteDTO(
         @Schema(description = "삭제하고 싶은 임시 저장 일기 고유 식별자(draftDiaryId)를 담는 List", example = "[1, 2]")
-        @NotNull List<Long> diaryIdList) {
+        @NotNull
+        List<Long> diaryIdList) {
 }
