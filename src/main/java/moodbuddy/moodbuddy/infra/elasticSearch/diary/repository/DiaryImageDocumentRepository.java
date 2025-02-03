3b62b9a8cdde67694124349353ef8a55e24a67f7
@_.mung <wjdahrrla@gmail.com>
2025-02-03 21:11:56 +0900

    Refactor: 엘라스틱서치 관련 로직 제거해라 - #69

--- src/main/java/moodbuddy/moodbuddy/infra/elasticSearch/diary/repository/DiaryImageDocumentRepository.java
+++
@@ -1,7 +0,0 @@
-package moodbuddy.moodbuddy.infra.elasticSearch.diary.repository;
-
-import moodbuddy.moodbuddy.infra.elasticSearch.diary.domain.DiaryImageDocument;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-public interface DiaryImageDocumentRepository extends ElasticsearchRepository<DiaryImageDocument, Long> {
-}
--- src/main/java/moodbuddy/moodbuddy/infra/elasticSearch/diary/service/DiaryDocumentService.java
+++
@@ -1,9 +0,0 @@
-package moodbuddy.moodbuddy.infra.elasticSearch.diary.service;
-
-import moodbuddy.moodbuddy.domain.diary.domain.Diary;
-import moodbuddy.moodbuddy.infra.elasticSearch.diary.domain.DiaryDocument;
-
-public interface DiaryDocumentService {
-    DiaryDocument save(Diary diary);
-    void delete(Long diaryId);
-}
